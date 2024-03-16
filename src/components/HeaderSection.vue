381c78b812019a36f9eddb6ab55429503d80430c
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-03-16 21:27:21 +0500

    Change header toggle button

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -27,7 +27,7 @@
       </div>
       <button class="site-header__sitenav-toggler" type="button">
         <span class="site-header__sitenav-toggler-inner">
-          <img :src="menu" aria-label="none">
+          <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="28px" height="28px"><path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z" fill="currentColor"/></svg>
         </span>
       </button>
     </div>
@@ -35,13 +35,8 @@
 </template>
 <script>
 import { RouterLink, RouterView } from 'vue-router'
-import {menu} from "../constants"
 export default {
-  data() {
-    return {
-      menu,
-    }
-  },
+
 }
 </script>
 <style>
