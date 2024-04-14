0c997530ca63d9fc5f98ad2ffddae2ab4a821f8a
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-04-14 13:51:55 +0500

    Delete data from app

--- src/components/App.vue
+++ src/components/App.vue
@@ -17,57 +17,57 @@ import HeaderSection from "./HeaderSection.vue"
 import FooterSection from "./FooterSection.vue"
 export default {
   components: {HeaderSection,FooterSection,},
-  data() {
-    return {
-      projects: [
-        {
-          id: 1,
-          img: 'https://github.com/Baxtibay/my-portfolio/blob/main/src/assets/projects-img/portfolio-Image.png?raw=true',
-          name: 'Restaurant',
-          githubLink: 'https://github.com/Baxtibay/restaurant',
-          deployLink: '',
-          infoLink: '',
-          texnologyes: ['Html', 'Css' ,'JavaScript'],
-        },
-        {
-          id: 2,
-          img: '',
-          name: 'Akademnashir',
-          githubLink: '',
-          deployLink: '',
-          infoLink: '',
-          texnologyes: ['Html', 'Css' ,'JavaScript'],
-        },
-        {
-          id: 3,
-          img: '',
-          name: 'Koffe roaster',
-          githubLink: '',
-          deployLink: '',
-          infoLink: '',
-          texnologyes: ['Html', 'Css' ,'JavaScript'],
-        },
-        {
-          id: 4,
-          img: '',
-          name: 'AnsorMed',
-          githubLink: '',
-          deployLink: '',
-          infoLink: '',
-          texnologyes: ['Html', 'Css' ,'JavaScript'],
-        },
-        {
-          id: 5,
-          img: '',
-          name: 'Countries',
-          githubLink: '',
-          deployLink: '',
-          infoLink: '',
-          texnologyes: ['Html', 'Css' ,'JavaScript'],
-        },
-      ]
-    }
-  }
+  // data() {
+  //   return {
+  //     projects: [
+  //       {
+  //         id: 1,
+  //         img: 'https://github.com/Baxtibay/my-portfolio/blob/main/src/assets/projects-img/portfolio-Image.png?raw=true',
+  //         name: 'Restaurant',
+  //         githubLink: 'https://github.com/Baxtibay/restaurant',
+  //         deployLink: '',
+  //         infoLink: '',
+  //         texnologyes: ['Html', 'Css' ,'JavaScript'],
+  //       },
+  //       {
+  //         id: 2,
+  //         img: '',
+  //         name: 'Akademnashir',
+  //         githubLink: '',
+  //         deployLink: '',
+  //         infoLink: '',
+  //         texnologyes: ['Html', 'Css' ,'JavaScript'],
+  //       },
+  //       {
+  //         id: 3,
+  //         img: '',
+  //         name: 'Koffe roaster',
+  //         githubLink: '',
+  //         deployLink: '',
+  //         infoLink: '',
+  //         texnologyes: ['Html', 'Css' ,'JavaScript'],
+  //       },
+  //       {
+  //         id: 4,
+  //         img: '',
+  //         name: 'AnsorMed',
+  //         githubLink: '',
+  //         deployLink: '',
+  //         infoLink: '',
+  //         texnologyes: ['Html', 'Css' ,'JavaScript'],
+  //       },
+  //       {
+  //         id: 5,
+  //         img: '',
+  //         name: 'Countries',
+  //         githubLink: '',
+  //         deployLink: '',
+  //         infoLink: '',
+  //         texnologyes: ['Html', 'Css' ,'JavaScript'],
+  //       },
+  //     ]
+  //   }
+  // }
 }
 
 </script>
--- src/pages/PortfolioPage.vue
+++ src/pages/PortfolioPage.vue
@@ -2,7 +2,74 @@
   <section class="projects">
     <h2 class="projects__heading">Portfolio</h2>
     <div class="projects__wrapper">
-      <Project v-for="project in projects" :project="project" />
+      <!-- <Project v-for="project in projects" :project="project" /> -->
+      <!-- PROJECT START -->
+      <div class="project-card">
+        <h3 class="project-card__heading">Akademnashir</h3>
+        <div class="project-card__img-wrapper">
+          <img class="project-card__img" src="/src/assets/projects-img/Akademnashr-preview.png" alt="project's picture" width="300" height="300">
+        </div>
+        <div class="project__links">
+          <a class="project__link" href ="https://github.com/Baxtibay/Akademnashr-pro" target="_blank">
+            <span class="project__link-text">Github</span>
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a class="project__link" href="https://akademnashr-project-site.netlify.app/" target="_blank">
+            <span class="project__link-text">Deploy</span>
+            <i class="fa-solid fa-globe"></i>
+          </a>
+          <routerLink class="project__link" to="akademnashir-info-page">
+            <span class="project__link-text">More info</span>
+            <i class="fa-solid fa-circle-info"></i>
+          </routerLink>
+        </div>
+        <ul class="project__texnalogy-list">
+          <li class="project__texnalogy-item">
+            <span class="project__texnalogy-name">JavaScript</span>
+          </li>
+          <li class="project__texnalogy-item">
+            <span class="project__texnalogy-name">Html</span>
+          </li>
+          <li class="project__texnalogy-item">
+            <span class="project__texnalogy-name">Css</span>
+          </li>
+        </ul>
+      </div>
+      <!-- PROJECT END -->
+
+      <!-- PROJECT START -->
+      <div class="project-card">
+        <h3 class="project-card__heading">AnsorMed</h3>
+        <div class="project-card__img-wrapper">
+          <img class="project-card__img" src="../assets/projects-img/AnsorMed-preview.png" alt="project's picture" width="300" height="300">
+        </div>
+        <div class="project__links">
+          <a class="project__link" href ="https://github.com/Baxtibay/AnsorMed" target="_blank">
+            <span class="project__link-text">Github</span>
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <routerLink class="project__link" to="/in-progress">
+            <span class="project__link-text">Deploy</span>
+            <i class="fa-solid fa-globe"></i>
+          </routerLink>
+          <routerLink class="project__link" to="/ansormed-info-page">
+            <span class="project__link-text">More info</span>
+            <i class="fa-solid fa-circle-info"></i>
+          </routerLink>
+        </div>
+        <ul class="project__texnalogy-list">
+          <li class="project__texnalogy-item">
+            <span class="project__texnalogy-name">JavaScript</span>
+          </li>
+          <li class="project__texnalogy-item">
+            <span class="project__texnalogy-name">Html</span>
+          </li>
+          <li class="project__texnalogy-item">
+            <span class="project__texnalogy-name">Css</span>
+          </li>
+        </ul>
+      </div>
+      <!-- PROJECT END -->
     </div>
   </section>
 </template>
